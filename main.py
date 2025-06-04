caf159ef35fc3a3053e8ae31ff0232e721a050d4
Fredy Santamaria <77696721+fsantamaria1@users.noreply.github.com>
2025-06-04 14:25:03 -0400

    Modularize main.py by creating database related function

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 """
 Main module of the time adjustment reminder script.
 """
-import os
-import sys
-from res.api import APIConnector
 from res.db.db_functions import (
     get_pay_period_by_start_date,
     get_worker_ids_with_missing_punches_by_pay_period
@@ -11,81 +8,49 @@ from res.db.db_functions import (
 from res.db.database import Database
 from res.date_util import DateUtil
 
-# Create a new database session
-db = Database()
-session = db.get_new_session()
 
-# Get the pay period for the previous week
-last_monday = DateUtil().get_last_monday()
-previous_week_pay_period = get_pay_period_by_start_date(
-    session,
-    last_monday
-)
+def fetch_pay_period(session):
+    """
+    Fetch the pay period for the previous week.
+    :param session: The database session.
+    :return: The pay period object for the previous week.
+    """
+    last_monday = DateUtil().get_last_monday()
+    return get_pay_period_by_start_date(session, last_monday)
 
-# Get employees with missing punches for the previous week's pay period
-worker_ids_with_missing_punches = get_worker_ids_with_missing_punches_by_pay_period(
-    session,
-    previous_week_pay_period.pay_period_id
-)
 
-# Get the environment variables for API key and brand ID
-API_KEY = os.getenv("SLICK_TEXT_API_KEY")
-if not API_KEY:
-    raise ValueError("SLICK_TEXT_API_KEY environment variable is not set.")
-BRAND_ID = os.getenv("SLICK_TEXT_BRAND_ID")
-if not BRAND_ID:
-    raise ValueError("SLICK_TEXT_BRAND_ID environment variable is not set.")
+def get_missing_punch_data(session, pay_period):
+    """
+    Get and log the worker IDs with missing punches for the specified pay period.
+    :param session: The database session.
+    :param pay_period: The pay period object.
+    :return: A list of worker IDs with missing punches.
+    """
+    worker_ids = get_worker_ids_with_missing_punches_by_pay_period(
+        session,
+        pay_period.pay_period_id
+    )
 
-# Initialize the API connector
-api_connector = APIConnector(token=API_KEY, brand_id=BRAND_ID)
+    return worker_ids
 
-# Fetch all contacts from the API
-contacts = api_connector.get_all_contacts(brand_id=BRAND_ID)
 
-# Filter contacts to find those with missing punches
-contact_IDs = []
-for contact in contacts:
-    contact_id = contact.get('contact_id')
-    first_name = contact.get('first_name')
-    last_name = contact.get('last_name')
-    email = contact.get('email')
-    phone_number = contact.get('mobile')
-    worker_id = contact.get('custom_fields', {}).get('adp_associate_id', None)
+def main():
+    """
+    Main function to run the time adjustment reminder script.
+    """
 
-    if worker_id in worker_ids_with_missing_punches:
-        # Add the contact ID to the list
-        contact_IDs.append(contact_id)
+    try:
+        db = Database()
+        with db.get_new_session() as session:
+            # Retrieve the pay period for the previous week
+            pay_period = fetch_pay_period(session)
 
-# If no contacts with missing punches are found, exit the script
-if not contact_IDs:
-    sys.exit()
+            # Get worker IDs with missing punches
+            get_missing_punch_data(session, pay_period)
 
-# Create a new contact list for the campaign
-CONTACT_LIST_AND_CAMPAIGN_NAME = (f"Time Adjustment Reminder "
-                                  f"{previous_week_pay_period.pay_period_start} - "
-                                  f"{previous_week_pay_period.pay_period_end}")
-new_contact_list = api_connector.create_contact_list(CONTACT_LIST_AND_CAMPAIGN_NAME)
-contact_list_id = new_contact_list.get("contact_list_id")
+    except Exception as e:
+        raise e
 
-# Add the contact IDs to the new list
-api_connector.add_contacts_to_list(contact_IDs, contact_list_id)
 
-# Define the message content for the campaign
-MESSAGE_CONTENT = """
-Good morning,
-
-This is a reminder that a time adjustment is needed on your timecard.
-Please review your ADP timecard and enter a KPA Time adjustment as soon as possible, if you haven't already done so.
-
-Thanks!
-
-"""
-
-# Create the campaign with the specified message content
-new_campaign = api_connector.create_campaign(
-    CONTACT_LIST_AND_CAMPAIGN_NAME,
-    MESSAGE_CONTENT,
-    contact_list_id
-)
-
-campaign_id = new_campaign.get("campaign_id")
+if __name__ == "__main__":
+    main()
